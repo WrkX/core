fca2987028c42ea252cff6291b3668ffbf5b1837
WrkX <pankratz.jonas95@gmail.com>
2023-11-23 09:53:42 +0100

    Players do not leave group on logout

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -730,12 +730,12 @@ void WorldSession::LogoutPlayer(bool Save)
         _player->CleanupChannels();
 
         // If the player is in a group (or invited), remove him. If the group if then only 1 person, disband the group.
-        _player->UninviteFromGroup();
+        //_player->UninviteFromGroup();
 
         // remove player from the group if he is:
         // a) in group; b) not in raid group; c) logging out normally (not being kicked or disconnected)
-        if (_player->GetGroup() && !_player->GetGroup()->isRaidGroup() && m_socket)
-            _player->RemoveFromGroup();
+        //if (_player->GetGroup() && !_player->GetGroup()->isRaidGroup() && m_socket)
+        //    _player->RemoveFromGroup();
 
         // Send update to group
         if (Group* group = _player->GetGroup())
